0c927b920a5766346b0672f25c6ed740dfcd8f7a
dsaitejreddy <d.saitejreddy@gmail.com>
2026-02-26 21:06:18 +0530

    UC2 Palindrome Checker implemented

--- PolinDrome/src/palindromeCheckerApp.java
+++ PolinDrome/src/palindromeCheckerApp.java
@@ -3,5 +3,6 @@ public class palindromeCheckerApp {
         System.out.println("welcome to pallen drome appp");
         System.out.println("version 1.0");
         System.out.println("System initialised");
+
     }
 }
