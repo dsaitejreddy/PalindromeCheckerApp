6d6e2cb9cd5561657f5fdcfae4ceb78acb14c713
dsaitejreddy <d.saitejreddy@gmail.com>
2026-02-27 09:05:23 +0530

    uc4 commited

--- PolinDrome/src/palindromeCheckerApp.java
+++ PolinDrome/src/palindromeCheckerApp.java
@@ -1,8 +1,29 @@
 public class palindromeCheckerApp {
-    static void main() {
-        System.out.println("welcome to pallen drome appp");
-        System.out.println("version 1.0");
-        System.out.println("System initialised");
+
+    public static void main(String[] args) {
+
+        String input = "radar";
+
+        char[] chars = input.toCharArray();
+
+        int start = 0;
+        int end = chars.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
 
     }
 }
